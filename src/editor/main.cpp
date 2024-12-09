9eba62dc3b47d4998e1bf402cba7412be1f44664
Zack Kollar <rallokkcaz@gmail.com>
2024-12-09 02:09:54 -0500

    :hammer: Convert initial editor setup to use the ECS system

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -4,8 +4,28 @@
 
 #include <engine/engine.h>
 
-int main(int argc, char *argv[]) {
+struct PlacementTool {
+  glm::vec2 gridSize;
+  glm::vec2 cursorPosition;
 
+  PlacementTool() : gridSize(64.0f, 64.0f), cursorPosition(0.0f, 0.0f) {}
+};
+
+struct Tools {
+  PlacementTool placementTool;
+};
+
+struct Level {
+  std::string name;
+};
+
+struct EditorState {
+  Level currentLevel;
+  Tools tools;
+};
+
+int main(int argc, char *argv[]) {
+  // Build a window
   auto window = ste::Window::builder()
                     .setTitle("Stabby Editor : v0.0.1")
                     .setSize(1280, 720)
@@ -17,6 +37,7 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
+  // Create an asset loader
   ste::AssetLoader::CreateInfo assetCreateInfo;
   auto assetLoader = ste::AssetLoader::create(assetCreateInfo);
   if (!assetLoader) {
@@ -24,8 +45,10 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
+  // Load a font
   auto font = assetLoader->load<ste::Font>("assets/fonts/better-vcr.ttf@16");
 
+  // Create a renderer
   ste::Renderer2D::CreateInfo rendererCreateInfo;
   auto renderer = ste::Renderer2D::create(rendererCreateInfo);
   if (!renderer) {
@@ -33,21 +56,82 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
+  // Setup the systems
   ste::TextRenderer textRenderer(*renderer);
   ste::AudioManager audioManager;
-  ste::InputManager inputManager;
-  ste::Camera2D camera(window->getWidth(), window->getHeight());
-  ste::GameTimer timer(120);
+  auto inputManager = std::make_shared<ste::InputManager>();
+
+  // World tools
+  auto camera =
+      std::make_shared<ste::Camera2D>(window->getWidth(), window->getHeight());
+  auto editorState = std::make_shared<EditorState>();
+  auto timer = std::make_shared<ste::GameTimer>(60.0f);
+
+  // Setup the world
   ste::World world;
 
-  glm::vec2 gridSize = {64.0f, 64.0f};
-  glm::vec2 cursorPosition = {0.0f, 0.0f};
+  // Add resources
+  world.addResource(camera);
+  world.addResource(editorState);
+  world.addResource(inputManager);
+  world.addResource(timer);
 
+  // Add systems
+  world.addSystem("Placement Tool", [](ste::World &world) {
+    auto editorState = world.getResource<EditorState>();
+    auto camera = world.getResource<ste::Camera2D>();
+    auto inputManager = world.getResource<ste::InputManager>();
+
+    auto &placementTool = editorState->tools.placementTool;
+
+    // Get the mouse position
+    glm::vec2 mousePosition = inputManager->getMousePosition();
+
+    // Get the world space cursor position
+    placementTool.cursorPosition =
+        camera->screenToWorld({mousePosition.x, mousePosition.y});
+
+    // Lock the cursor to a grid
+    placementTool.cursorPosition.x =
+        std::floor(placementTool.cursorPosition.x / placementTool.gridSize.x) *
+        placementTool.gridSize.x;
+    placementTool.cursorPosition.y =
+        std::floor(placementTool.cursorPosition.y / placementTool.gridSize.y) *
+        placementTool.gridSize.y;
+  });
+
+  // Add a render system
+  world.addSystem(
+      "Render Tools",
+      [&renderer](ste::World &world) {
+        auto camera = world.getResource<ste::Camera2D>();
+        auto editorState = world.getResource<EditorState>();
+
+        auto &placementTool = editorState->tools.placementTool;
+
+        // Begin drawing the scene
+        renderer->beginScene(camera->getViewProjectionMatrix());
+
+        // Draw the a font
+        renderer->drawQuad(
+            {placementTool.cursorPosition.x + (placementTool.gridSize.x / 2.0f),
+             placementTool.cursorPosition.y + (placementTool.gridSize.y / 2.0f),
+             0.0f},
+            {placementTool.gridSize.x, placementTool.gridSize.y},
+            {1.0f, 1.0f, 1.0f, 0.1f}, 0.0f, 0.1f, {1.0f, 1.0f, 1.0f, 1.0f});
+
+        // End drawing the scene
+        renderer->endScene();
+      },
+      0, true);
+
+  // Editor loop
   bool running = true;
   while (running) {
     // Update the game timer
-    timer.update();
+    timer->update();
 
+    // Handle events
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
       switch (event.type) {
@@ -57,38 +141,17 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    inputManager.update();
+    // Update
+    inputManager->update();
+    world.update(timer->getDeltaTime());
 
+    // Render
     window->clearColor(0.361f, 0.361f, 0.471f, 1.0f);
-
-    renderer->beginScene(camera.getViewProjectionMatrix());
-
-    // Get the mouse position
-    glm::vec2 mousePosition = inputManager.getMousePosition();
-
-    // Get the world space cursor position
-    cursorPosition = camera.screenToWorld({mousePosition.x, mousePosition.y});
-
-    // Lock the cursor to a grid
-    cursorPosition.x = std::floor(cursorPosition.x / gridSize.x) * gridSize.x;
-    cursorPosition.y = std::floor(cursorPosition.y / gridSize.y) * gridSize.y;
-
-    // Draw the a font
-    std::string fps = "FPS: " + std::to_string(timer.getFPS());
-    textRenderer.renderText(*font, fps, {12.0f, window->getHeight() - 28.0f},
-                            {1.0f, 1.0f, 1.0f, 1.0f});
-
-    // Draw a white quad
-    renderer->drawQuad({cursorPosition.x + (gridSize.x / 2.0f),
-                        cursorPosition.y + (gridSize.y / 2.0f), 0.0f},
-                       {gridSize.x, gridSize.y}, {1.0f, 1.0f, 1.0f, 0.0f}, 0.0f,
-                       0.1f, {1.0f, 1.0f, 1.0f, 1.0f});
-
-    renderer->endScene();
+    world.render();
     window->swapBuffers();
 
     // Cap the frame rate
-    timer.limitFrameRate();
+    timer->limitFrameRate();
   }
 
   return 0;
